51a670d9c9510b338f37212a5695a9f497f61833
RIMPA DAS <sujata@itobuz.com>
2022-05-01 12:39:17 +0530

    fix: baseURL issue

--- src/Api.ts
+++ src/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.baseURL,
+  baseURL: "https://parseapi.back4app.com",
   headers: {
     "Accept": "application/json",
     "Content-type": "application/json",
